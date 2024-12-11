a412727ebc5cf947e4825262b267c8a42c4f069f
Peep Noorkõiv <peepnoor@gmail.com>
2024-12-11 21:18:45 +0200

    Make player move and add tiled background

--- src/gameLayer/gameLayer.cpp
+++ src/gameLayer/gameLayer.cpp
@@ -1,13 +1,25 @@
 #define GLM_ENABLE_EXPERIMENTAL
 #include "gameLayer.h"
 #include <glad/glad.h>
+
+#include <platformInput.h>
 #include <glm/gtx/transform.hpp>
 #include <gl2d/gl2d.h>
+#include <tiledRenderer.h>
 
+struct GameplayData
+{
+	glm::vec2 playerPos = {100, 100};
+};
+
+GameplayData data;
 
 gl2d::Renderer2D renderer;
 
 gl2d::Texture spaceShipTexture;
+gl2d::Texture backgroundTexture;
+
+TiledRenderer tiledRenderer;
 
 bool initGame()
 {
@@ -16,7 +28,10 @@ bool initGame()
 	renderer.create();
 
 	spaceShipTexture.loadFromFile(RESOURCES_PATH "spaceShip/ships/green.png", true);
-	
+	backgroundTexture.loadFromFile(RESOURCES_PATH "background1.png", true);
+
+	tiledRenderer.texture = backgroundTexture;
+
 	return true;
 }
 
@@ -35,9 +50,54 @@ bool gameLogic(float deltaTime)
 	renderer.updateWindowMetrics(w, h);
 #pragma endregion
 
+#pragma region movement
 
+	glm::vec2 move = {};
 
-	renderer.renderRectangle({100,100, 100, 100}, spaceShipTexture);
+	if (
+		platform::isButtonHeld(platform::Button::Left) ||
+		platform::isButtonHeld(platform::Button::A)
+		) {
+		move.x = -1;
+	}
+	if (
+		platform::isButtonHeld(platform::Button::Right) ||
+		platform::isButtonHeld(platform::Button::D)
+		) {
+		move.x = 1;
+	}
+	if (
+		platform::isButtonHeld(platform::Button::Up) ||
+		platform::isButtonHeld(platform::Button::W)
+		) {
+		move.y = -1;
+	}
+	if (
+		platform::isButtonHeld(platform::Button::Down) ||
+		platform::isButtonHeld(platform::Button::S)
+		) {
+		move.y = 1;
+	}
+
+	if (move.x != 0 || move.y != 0) {
+		move = glm::normalize(move);
+
+		move *= deltaTime * 1000;
+		data.playerPos += move;
+	}
+
+#pragma endregion
+
+#pragma region render background
+
+	renderer.currentCamera.zoom = 0.5;
+	tiledRenderer.render(renderer);
+
+#pragma endregion
+
+	renderer.currentCamera.follow(data.playerPos, deltaTime * 450, 10, 200, w, h);
+
+	renderer.renderRectangle({data.playerPos, 100, 100}, spaceShipTexture);
 
 
 	renderer.flush();
